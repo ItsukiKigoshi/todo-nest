242a641264ddb0a73012497634d8b9334c4a0eeb
ItsukiKigoshi <itsukikigoshi@gmail.com>
2024-01-13 11:47:55 +0900

    Add Swagger documentation to the API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,15 @@ async function bootstrap() {
       //options here
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('NesToDo API')
+    .setDescription('The NesToDo API description')
+    .setVersion('1.0')
+    .addTag('NesToDo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
